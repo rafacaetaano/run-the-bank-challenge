8d31f4bd9f6b58a043772e1a19a1e26f03ebdaeb
Rafael Ferreira Caetano <rcaetano0709@gmail.com>
2024-02-09 15:15:34 -0300

    Exclusão de anotação que foi usada para teste

--- src/main/java/br/com/runthebank/entity/Account.java
+++ src/main/java/br/com/runthebank/entity/Account.java
@@ -1,7 +1,6 @@
 package br.com.runthebank.entity;
 
 import jakarta.persistence.*;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,7 +8,6 @@ import lombok.Setter;
 @Table(name = "ACCOUNTS")
 @Getter
 @Setter
-@EqualsAndHashCode(of = "id")
 public class Account {
 	
 	@Id
